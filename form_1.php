7cf933088e24c04beedbf7f2154378f0a47f9bfb
unknown <jeremyprinceandaya07@gmail.com>
2024-01-23 12:15:13 +0800

    New file

---
+++ form_1.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["firstname"] , $_POST["lastname"]; ?><br>
+    
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your address is: <?php echo $_POST["address"]; ?>
+
+</body>
+</html>
